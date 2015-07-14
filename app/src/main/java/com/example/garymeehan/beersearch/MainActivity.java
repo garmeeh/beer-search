7b0f119d82d9a7bfeebc99f8d5eb26948234a6d2
Gary Meehan <gary@emagine.ie>
2015-07-14 21:20:27 +0100

    Added persintance with welcome msg, prompt for name on first run

--- app/src/main/java/com/example/garymeehan/beersearch/MainActivity.java
+++ app/src/main/java/com/example/garymeehan/beersearch/MainActivity.java
@@ -1,17 +1,57 @@
 package com.example.garymeehan.beersearch;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ShareActionProvider;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    //Declarations
+    Button mainButton;
+    //search text
+    EditText mainEditText;
+    //list view
+    ListView mainListView;
+    ArrayList mBeerList = new ArrayList();
+    SharedPreferences mSharedPreferences;
+    ShareActionProvider mShareActionProvider;
+    ProgressDialog mDialog;
+
+    //welcome variables
+    private static final String PREFS = "prefs";
+    private static final String PREF_NAME = "name";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Access button defined in xml
+        mainButton = (Button) findViewById(R.id.main_button);
+        // and listen for it here
+        mainButton.setOnClickListener(this);
+
+        //acccess search text in xml
+        mainEditText = (EditText) findViewById(R.id.main_edittext);
+
+        //access list view
+        mainListView = (ListView) findViewById(R.id.main_listview);
+
+        displayWelcome();
     }
 
     @Override
@@ -35,4 +75,59 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    //Click listener
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    public void displayWelcome() {
+
+        // Access the device's key-value storage
+        mSharedPreferences = getSharedPreferences(PREFS, MODE_PRIVATE);
+        // Read the user's name,
+        // or an empty string if nothing found
+        String name = mSharedPreferences.getString(PREF_NAME, "");
+
+        if (name.length() > 0) {
+            // If the name is valid, display a Toast welcoming them
+            Toast.makeText(this, "Welcome back, " + name + "!", Toast.LENGTH_LONG).show();
+        }else {
+            // otherwise, show a dialog to ask for their name
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("Welcome");
+            alert.setMessage("What is your name?");
+
+            // Create EditText for entry
+            final EditText input = new EditText(this);
+            alert.setView(input);
+
+            // Make an "OK" button to save the name
+            alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                public void onClick(DialogInterface dialog, int whichButton) {
+
+                    // Grab the EditText's input
+                    String inputName = input.getText().toString();
+                    // Put it into memory (don't forget to commit!)
+                    SharedPreferences.Editor e = mSharedPreferences.edit();
+                    e.putString(PREF_NAME, inputName);
+                    e.commit();
+                    // Welcome the new user
+                    Toast.makeText(getApplicationContext(), "Welcome, " + inputName + "!", Toast.LENGTH_LONG).show();
+                }
+            });
+
+            // Make a "Cancel" button
+            // that simply dismisses the alert
+            alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                public void onClick(DialogInterface dialog, int whichButton) {}
+            });
+
+            alert.show();
+        }
+    }//end welcome
+
 }
